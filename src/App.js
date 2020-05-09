f014be8846a80378713d9baee457612054a124ec
fors0 <jensfors0@gmail.com>
2020-05-09 10:32:52 +0200

    create-react-app + added a pokecard

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Pokecard from './Components/Pokecard';
+
+function App() {
+  return (
+    <div className="App">
+      <Pokecard></Pokecard>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Pokecard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Pokecard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Pokecard! :D </h1>
+      </div>
+    )
+  }
+}
+
+export default Pokecard
