b36b76270f628afd009a76946580b8be20d98780
fors0 <jensfors0@gmail.com>
2020-05-09 13:24:22 +0200

    switched to another api with better pokemon pictures

--- src/Components/Pokecard.js
+++ src/Components/Pokecard.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import './Pokecard.css';
-const POKE_API='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const POKE_API='https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
+// if the number smaller than 999, then add 00 in front of id.
+// slice to take last 3 digits.
+// this ensures there is always 3 digits for the API. e.g 001, 011, 111
+let getThreeDigits = (number) => (number <= 999 ? `00${number}`.slice(-3) : number)
 
 export class Pokecard extends Component {
   render() {
-    let imgSrc = `${POKE_API}${this.props.id}.png`;
+    let imgSrc = `${POKE_API}${getThreeDigits(this.props.id)}.png`;
     return (
       <div className="Pokecard">
         <h1 className="Pokecard-title">{this.props.name}</h1>
